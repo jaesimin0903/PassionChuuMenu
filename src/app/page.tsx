4bc7b885552bf9afab8f7813dcb3904dd99b2187
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2024-05-28 02:43:55 +0900

    feat : add a git visitor check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,8 @@ const Menu = ({ params }: { params: { locale: Language } }) => {
     return (
         <div className="container mx-auto px-4 py-6 space-y-6">
             <h1 className="text-2xl font-bold text-center">열정도 쭈꾸미 메뉴판</h1>
+            <center><a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fpassion-chuu-menu.vercel.app&count_bg=%23E3A034&title_bg=%23555555&icon=devrant.svg&icon_color=%23E7E7E7&title=hits&edge_flat=true"/></a></center>
+
             <div className="text-normal text-center">SELECT LANGUAGE</div>
             <div className="flex justify-center">
                 <MenuDropdown
@@ -28,6 +30,7 @@ const Menu = ({ params }: { params: { locale: Language } }) => {
             <MenuList selectedLanguage={selectedLanguage} />
             <ScrollButtons />
             <HowtoeatButton/>
+
         </div>
     );
 };
